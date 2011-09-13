c51e23eb715f06fce8e4fa8b728c44358ed75293
Steven Barker <BlckKnght@gmail.com>
2011-09-12 20:23:41 -0700

    Add lots of docstrings and comments to the code.

--- infinity.py
+++ infinity.py
@@ -4,52 +4,83 @@ from __future__ import division
 import logging
 
 def binomial_coefficient(n, k):
-    if k > n-k:
+    """Returns the binomial coefficient, "n choose k"."""
+    if k > n-k: # if k is greater than n/2, swap it for n-k which will be
         k = n-k
 
     result = 1;
-    for i in range(k):
-        result *= (n-i) / (i+1)
+    for i in range(k): # loop from 0 to k-1
+        result *= (n-i) / (i+1) # multiply in one term of the result
 
     return result
 
   
 def normal_roll(dice, target):
-    if target <= 0:
-        return [1] + [0]*dice
-    
-    success_odds = target / 20
+    """Calculate the results of a normal roll.
+
+    Returns a list with dice-1 probabilities, the odds of getting a given
+    number of successes (the number of successes is the index to the list).
+    """
+    if target <= 0: # a negative target number means you always fail
+        return [1] + [0]*dice # return a list with a 1 followed by "dice" 0s
+
+    success_odds = target / 20 # odds of succeeding on each die
     successes = []
+
+    # the odds of success follow a binomial distribution
     for i in range(dice+1):
-        coef = binomial_coefficient(dice, i)
+        coef = binomial_coefficient(dice, i) # binomial coeficient
         successes.append(coef * success_odds**i * (1-success_odds)**(dice-i))
+
     return successes
 
 
 def normal_roll_with_crits(dice, target):
+    """Calculate how many successes and critical successes there are.
+
+    Returns a list of lists, with each entry in the outer list coresponding
+    to a number of successes (of all kinds) and the inner list showing how
+    many of them are critical successes.
+
+    If the result is stored in R, R[0][0] is the probability of failure,
+    R[3][0] is the probability of three successes with no criticals, R[2][2] is
+    the odds of two successes that are both criticals.
+    """
     if target <= 0:
         return [[1]] + [[0]*(d+2) for d in range(dice)]
-    
-    successes = normal_roll(dice, target)
-    crit_odds = 1/target
+
+    successes = normal_roll(dice, target) # calculate results without crits
+    crit_odds = 1/target # odds of any given success being a crit
     results = []
+    
+    # number of crits (for a number of successes) is a binomial distribution
     for s in range(len(successes)):
         crits = []
         for c in range(s+1):
             coef = binomial_coefficient(s, c)
             crits.append(successes[s] * coef *
                          crit_odds**c * (1-crit_odds)**(s-c))
-            
+
         results.append(crits)
-        
+
     return results
 
 
 def armor_save(hits, damage):
+    """Calculate the odds of passing an armor save against a given damage.
+
+    Returns a list of probabilities of failing a given number of saves.
+    
+    The "hits" parameter should be formatted like the return value of the
+    normal_roll_with_crits function (a list of lists) as criticals hits skip
+    the armor save.
+    """
     results = [0]*len(hits)
 
     #print(hits)
-    
+
+    # Failing an armor save is equivalent to succeeding in a normal roll
+    # against the damage value. Crits of course skip the save.
     for h in range(len(hits)):
         for c in range(len(hits[h])):
             wounds = normal_roll(h-c, damage)
@@ -69,37 +100,55 @@ AP_EXP_AMMO = EXP_AMMO | AP_AMMO #6
 AP_T2_AMMO  = T2_AMMO | AP_AMMO  #7
 
 def armor_save_with_special_ammo(hits, damage, ammo):
-    
+    """Calculate armor saves against some special ammo (DA, EXP, T2).
+
+    Returns the odds of taking a given number of wounds.
+
+    AP ammo needs to be handled separately, as it affects the modified
+    damage value, rather than how hits themselves get processed.
+    """
     if ammo % AP_AMMO == DA_AMMO:
         new_hits = [hits[0]] + [[0]*(d+2) for d in range((len(hits)-1)*2)]
         for h in range(1, len(hits)):
             for c in range(h+1): # crits count as one crit and one regular hit
                 new_hits[2*h][c] += hits[h][c]
-                
+
         hits = new_hits
-        
+
     elif ammo % AP_AMMO == EXP_AMMO:
         new_hits = [hits[0]] + [[0]*(d+2) for d in range((len(hits)-1)*3)]
         for h in range(1, len(hits)):
             for c in range(h+1): # crits count as one crit and two regular hits
                 new_hits[3*h][c] += hits[h][c]
-                
+
         hits = new_hits
 
+    # calculate armor saves like normal
     results = armor_save(hits, damage)
 
     if ammo % AP_AMMO == T2_AMMO:
+        # double damage by inserting zeros for all the odd positions
         for i in range(len(results)-1, 0, -1):
-            results.insert(i, 0) # double damage by inserting zeros for all the odd positions
+            results.insert(i, 0)
 
     return results
 
 
 def normal_roll_and_save(B, BS, damage, ammo=0):
-    return armor_save_with_special_ammo(normal_roll_with_crits(B, BS), damage, ammo)
+    """Feed the result of a normal roll into the armor save functions."""
+    return armor_save_with_special_ammo(normal_roll_with_crits(B, BS),
+                                        damage, ammo)
 
 
 def ftf_1vN_roll_with_crits(defender_target, attacker_dice, attacker_target):
+    """Calculate the results of a face to face roll between a defender who
+    rolls 1 die and an attacker who rolls 1 or more.
+
+    Returns a list with three values: the odds of a draw (or both failing),
+    the odds of the defender winning (as a list of lists, as in the
+    normal_roll_with_crits function, but with the first value removed), and
+    the probability of the attacker winning (same format).
+    """
     nothing_happens = 0
     defender_hits = [0, 0]
     attacker_hits = [[0] * (i+2) for i in range(attacker_dice)]
@@ -190,6 +239,11 @@ def ftf_1vN_roll_with_crits(defender_target, attacker_dice, attacker_target):
 
 
 def gather_crits(roll):
+    """Strip out multiple successes.
+
+    Returns the probabilities of failure, of getting at least one success
+    (without any crits) and the odds of getting at least one crit.
+    """
     hit = 0
     crit = 0
     for hits in range(1, len(roll)):
@@ -200,22 +254,13 @@ def gather_crits(roll):
 
 
 def ignore_crits(roll):
+    """Strip out all info about crits and multiple successes.
+
+    Returns the probabilty of failure and the probability of success.
+    """
     successes = sum(sum(roll[i]) for i in range(1, len(roll)))
 
     return roll[0][0], successes
-
-
-def ftf_1vN_roll_and_save(defender_target, defender_damage, defender_ammo,
-                          attacker_dice, attacker_target,
-                          attacker_damage, attacker_ammo):
-    hits = ftf_1vN_roll_with_crits(defender_target, attacker_dice, attacker_target)
-
-    defender_wounds = armor_save_with_special_ammo([[0]] + hits[1], defender_damage, defender_ammo)
-    attacker_wounds = armor_save_with_special_ammo([[0]] + hits[2], attacker_damage, attacker_ammo)
-
-    no_wounds = hits[0] + defender_wounds[0] + attacker_wounds[0]
-
-    return no_wounds, defender_wounds[1:], attacker_wounds[1:]
 
 
 def resolve_test(attacker_action, attacker_dice,
@@ -226,7 +271,15 @@ def resolve_test(attacker_action, attacker_dice,
                  defender_target, defender_target_mod,
                  defender_damage, defender_ammo,
                  defender_armor, defender_cover):
+    """Figure out what sort of roll is being requested by the user, then
+    run the test and report the results.
 
+    Returns three values, the odds of both sides failing, a list with the
+    probabilities of the defender's results, and a list of the attacker's
+    results. The lists can be indexed by numbers of wounds given, successes
+    vs critical successes, or there may be just a single success value,
+    depending on what sort of test was run.
+    """
     a_dice = attacker_dice if attacker_action == "attack" else 1
     d_cover = 0 if attacker_action != "attack" or not defender_cover else 3
     a_cover = 0 if defender_action != "attack" or not attacker_cover else 3
