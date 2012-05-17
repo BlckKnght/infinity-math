18de06dacea9e90054d3f34cac0a41db61321688
Steven Barker <BlckKnght@gmail.com>
2012-05-17 03:50:31 -0700

    Switch to Jynja2 templates.

--- django_settings.py
+++
@@ -1,4 +0,0 @@
-# django_settings.py
-
-# nothing to do here, we'll take all default values,
-# since we're only using Django for templates.
--- home.py
+++ home.py
@@ -1,12 +1,16 @@
 # home.py
 
-import webapp2 as webapp
-from google.appengine.ext.webapp import template
+import webapp2 as webapp2
+from webapp2_extras import jinja2
 import logging
 
 from infinity import *
 
-class HomeHandler(webapp.RequestHandler):
+class HomeHandler(webapp2.RequestHandler):
+    @webapp2.cached_property
+    def jinja2(self):
+        return jinja2.get_jinja2(app=self.app)
+
     def get(self):
         attacker_action = self.request.get("attacker_action")
         attacker_target = int(self.request.get("attacker_target", 11))
@@ -77,4 +81,4 @@ class HomeHandler(webapp.RequestHandler):
         
         #logging.info(args)
         
-        self.response.out.write(template.render("templates/home.html", args))
+        self.response.out.write(self.jinja2.render_template("home.html", **args))
